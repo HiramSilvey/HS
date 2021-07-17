d765466fdcc740fa98db409e7e102a2b8e183c48
Hiram Silvey <blujay270@gmail.com>
2021-07-16 21:00:18 -0400

    Fixed build.sh after reorganizing file structure.

--- main/build.sh
+++ main/build.sh
@@ -5,6 +5,6 @@
 # path to the directory containing the nanopb protoc binary.
 
 src_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd "$src_dir/../.."
+cd "$src_dir/.."
 protoc="$1/protoc"
 $protoc --nanopb_out=$src_dir profile.proto
