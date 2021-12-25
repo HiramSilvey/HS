fe271175039afc4b96cb550a63daa9ce7a63c730
Hiram Silvey <blujay270@gmail.com>
2021-12-24 22:57:34 -0500

    First successful test case complete!

---
+++ main/test.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+# Copyright 2021 Hiram Silvey
+
+src_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $src_dir/build
+cmake .. && cmake --build . --verbose && ./pc_controller_test
--- main/test/pc_controller_test.cpp
+++ main/test/pc_controller_test.cpp
@@ -8,18 +8,124 @@
 #include "mock_teensy.h"
 
 using ::testing::_;
+using ::testing::AtLeast;
 using ::testing::Return;
 
-TEST(PCControllerTest, GetDPadAngleUp) {
-  auto teensy = std::make_unique<MockTeensy>();
+class PCControllerTest : public ::testing::Test {
+protected:
+  PCControllerTest() {
+    teensy_ = std::make_unique<MockTeensy>();
 
-  EXPECT_CALL(*teensy, DigitalReadLow(_)).WillOnce(Return(true));
+    EXPECT_CALL(*teensy_, DigitalReadLow)
+      .Times(AtLeast(1));
+    EXPECT_CALL(*teensy_, EEPROMRead)
+      .Times(AtLeast(1));
+    EXPECT_CALL(*teensy_, Exit);
+    EXPECT_CALL(*teensy_, JoystickUseManualSend);
+  }
+  std::unique_ptr<MockTeensy> teensy_;
+};
 
-  PCController controller(std::move(teensy));
+TEST_F(PCControllerTest, GetDPadAngle) {
+  const uint8_t pin = 1;
 
-  const PCButtonPinMapping mapping = {
-      .hat_up = {1},
+  EXPECT_CALL(*teensy_, DigitalReadLow(pin))
+    .WillRepeatedly(Return(true));
+
+  PCController controller(std::move(teensy_));
+
+  PCButtonPinMapping mapping;
+  EXPECT_EQ(controller.GetDPadAngle(mapping), -1);
+
+  mapping = {
+    .hat_right = {pin},
   };
+  EXPECT_EQ(controller.GetDPadAngle(mapping), 90);
 
+  mapping = {
+    .hat_left = {pin},
+  };
+  EXPECT_EQ(controller.GetDPadAngle(mapping), 270);
+
+  mapping = {
+    .hat_left = {pin},
+    .hat_right = {pin},
+  };
+  EXPECT_EQ(controller.GetDPadAngle(mapping), -1);
+
+  mapping = {
+    .hat_down = {pin},
+  };
+  EXPECT_EQ(controller.GetDPadAngle(mapping), 180);
+
+  mapping = {
+    .hat_down = {pin},
+    .hat_right = {pin},
+  };
+  EXPECT_EQ(controller.GetDPadAngle(mapping), 135);
+
+  mapping = {
+    .hat_down = {pin},
+    .hat_left = {pin},
+  };
+  EXPECT_EQ(controller.GetDPadAngle(mapping), 225);
+
+  mapping = {
+    .hat_down = {pin},
+    .hat_left = {pin},
+    .hat_right = {pin},
+  };
+  EXPECT_EQ(controller.GetDPadAngle(mapping), 180);
+
+  mapping = {
+      .hat_up = {pin},
+  };
   EXPECT_EQ(controller.GetDPadAngle(mapping), 0);
+
+  mapping = {
+    .hat_up = {pin},
+    .hat_right = {pin},
+  };
+  EXPECT_EQ(controller.GetDPadAngle(mapping), 45);
+
+  mapping = {
+    .hat_up = {pin},
+    .hat_left = {pin},
+  };
+  EXPECT_EQ(controller.GetDPadAngle(mapping), 315);
+
+  mapping = {
+    .hat_up = {pin},
+    .hat_left = {pin},
+    .hat_right = {pin},
+  };
+  EXPECT_EQ(controller.GetDPadAngle(mapping), 0);
+
+  mapping = {
+    .hat_up = {pin},
+    .hat_down = {pin},
+  };
+  EXPECT_EQ(controller.GetDPadAngle(mapping), -1);
+
+  mapping = {
+    .hat_up = {pin},
+    .hat_down = {pin},
+    .hat_right = {pin},
+  };
+  EXPECT_EQ(controller.GetDPadAngle(mapping), 90);
+
+  mapping = {
+    .hat_up = {pin},
+    .hat_down = {pin},
+    .hat_left = {pin},
+  };
+  EXPECT_EQ(controller.GetDPadAngle(mapping), 270);
+
+  mapping = {
+    .hat_up = {pin},
+    .hat_down = {pin},
+    .hat_left = {pin},
+    .hat_right = {pin},
+  };
+  EXPECT_EQ(controller.GetDPadAngle(mapping), -1);
 }
