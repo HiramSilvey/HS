602292ab99ec59fd5bc1455de64ebc2cc30ea24d
Hiram Silvey <blujay270@gmail.com>
2022-01-02 00:54:26 -0500

    Added Joystick calibration tests.

--- main/test/configurator_test.cpp
+++ main/test/configurator_test.cpp
@@ -53,13 +53,12 @@ TEST(ConfiguratorTest, FetchJoystickCoords) {
 
   {
     InSequence seq;
-
     EXPECT_CALL(teensy, UpdateHallData);
     EXPECT_CALL(teensy, GetHallZ).WillOnce(Return(0.1));
     EXPECT_CALL(teensy, GetHallX).WillOnce(Return(0.2));
-    uint8_t expected_x[4] = {0, 30, 132, 128};
+    uint8_t expected_x[4] = {0, 30, 132, 128};  // 2000000
     EXPECT_CALL(teensy, GetHallY).WillOnce(Return(0.3));
-    uint8_t expected_y[4] = {0, 45, 198, 192};
+    uint8_t expected_y[4] = {0, 45, 198, 192};  // 3000000
 
     EXPECT_CALL(teensy, SerialWrite(_, 4))
         .With(Args<0, 1>(ElementsAreArray(expected_x)));
@@ -68,6 +67,68 @@ TEST(ConfiguratorTest, FetchJoystickCoords) {
   }
 
   configurator::internal::FetchJoystickCoords(teensy);
+}
+
+TEST(ConfiguratorTest, CalibrateJoystick_RangeX) {
+  MockTeensy teensy;
+
+  EXPECT_CALL(teensy, Millis)
+      .Times(4)
+      .WillOnce(Return(0))
+      .WillOnce(Return(0))
+      .WillOnce(Return(0))
+      .WillOnce(Return(15000));
+  EXPECT_CALL(teensy, UpdateHallData).Times(2);
+  EXPECT_CALL(teensy, GetHallZ).Times(2).WillRepeatedly(Return(0.1));
+  EXPECT_CALL(teensy, GetHallX)
+      .Times(2)
+      .WillOnce(Return(0.3))
+      .WillOnce(Return(-0.4));
+  EXPECT_CALL(teensy, GetHallY)
+      .Times(2)
+      .WillOnce(Return(0.2))
+      .WillOnce(Return(-0.3));
+  uint8_t center[4] = {255, 248, 94, 224};  // -500000
+  uint8_t range[4] = {0, 53, 103, 224};     // 3500000
+
+  EXPECT_CALL(teensy, SerialWrite(_, 4))
+      .With(Args<0, 1>(ElementsAreArray(center)))
+      .Times(2);
+  EXPECT_CALL(teensy, SerialWrite(_, 4))
+      .With(Args<0, 1>(ElementsAreArray(range)));
+
+  configurator::internal::CalibrateJoystick(teensy);
+}
+
+TEST(ConfiguratorTest, CalibrateJoystick_RangeY) {
+  MockTeensy teensy;
+
+  EXPECT_CALL(teensy, Millis)
+      .Times(4)
+      .WillOnce(Return(0))
+      .WillOnce(Return(0))
+      .WillOnce(Return(0))
+      .WillOnce(Return(15000));
+  EXPECT_CALL(teensy, UpdateHallData).Times(2);
+  EXPECT_CALL(teensy, GetHallZ).Times(2).WillRepeatedly(Return(0.1));
+  EXPECT_CALL(teensy, GetHallX)
+      .Times(2)
+      .WillOnce(Return(0.2))
+      .WillOnce(Return(-0.3));
+  EXPECT_CALL(teensy, GetHallY)
+      .Times(2)
+      .WillOnce(Return(0.3))
+      .WillOnce(Return(-0.4));
+  uint8_t center[4] = {255, 248, 94, 224};  // -500000
+  uint8_t range[4] = {0, 53, 103, 224};     // 3500000
+
+  EXPECT_CALL(teensy, SerialWrite(_, 4))
+      .With(Args<0, 1>(ElementsAreArray(center)))
+      .Times(2);
+  EXPECT_CALL(teensy, SerialWrite(_, 4))
+      .With(Args<0, 1>(ElementsAreArray(range)));
+
+  configurator::internal::CalibrateJoystick(teensy);
 }
 
 }  // namespace hs
