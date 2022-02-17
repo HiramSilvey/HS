64387779601cee416400b43bdb1901642eb6bd35
Hiram Silvey <blujay270@gmail.com>
2022-02-16 23:34:58 -0500

    Add build command for windows binary.

--- configurator/build.sh
+++ configurator/build.sh
@@ -4,3 +4,4 @@ src_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$src_dir/src"
 
 cargo +nightly build -Z configurable-env
+cargo +nightly build -Z configurable-env --target x86_64-pc-windows-gnu
