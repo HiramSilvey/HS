3dcfb0a48f656fd52d5939797bd64f7c4629162b
Hiram Silvey <blujay270@gmail.com>
2022-02-27 13:02:16 -0500

    Upon updating rustup, +nightly and configurable-env build/run flags are no longer needed. 'cargo build' and 'cargo run' can now be used as-is.

--- configurator/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-src_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd "$src_dir/src"
-
-cargo +nightly build -Z configurable-env
-cargo +nightly build -Z configurable-env --target x86_64-pc-windows-gnu
