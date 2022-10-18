3b365345e1eb03f49ec1298e1937b255de99ab53
Hiram Silvey <blujay270@gmail.com>
2022-10-18 00:33:39 -0400

    Pins used to determine profiles and configurator mode weren't pulled up and given the proper time to stabilize. Added an explicit InitPins function to ensure pin readings at startup are as consistent as possible, with the hope of fixing any false readings causing incorrect startup configurations.

--- main/main.ino
+++ main/main.ino
@@ -14,7 +14,29 @@ std::unique_ptr<hs::Controller> controller;
 extern uint8_t nsgamepad_active;
 extern volatile uint8_t usb_configuration;
 
+void InitPins() {
+  pinMode(hs::pins::kThumbTop, INPUT_PULLUP);
+  pinMode(hs::pins::kThumbMiddle, INPUT_PULLUP);
+  pinMode(hs::pins::kThumbBottom, INPUT_PULLUP);
+  pinMode(hs::pins::kIndexTop, INPUT_PULLUP);
+  pinMode(hs::pins::kIndexMiddle, INPUT_PULLUP);
+  pinMode(hs::pins::kMiddleTop, INPUT_PULLUP);
+  pinMode(hs::pins::kMiddleMiddle, INPUT_PULLUP);
+  pinMode(hs::pins::kMiddleBottom, INPUT_PULLUP);
+  pinMode(hs::pins::kRingTop, INPUT_PULLUP);
+  pinMode(hs::pins::kRingMiddle, INPUT_PULLUP);
+  pinMode(hs::pins::kRingBottom, INPUT_PULLUP);
+  pinMode(hs::pins::kPinkyTop, INPUT_PULLUP);
+  pinMode(hs::pins::kPinkyMiddle, INPUT_PULLUP);
+  pinMode(hs::pins::kPinkyBottom, INPUT_PULLUP);
+  pinMode(hs::pins::kLeftOuter, INPUT_PULLUP);
+  pinMode(hs::pins::kLeftInner, INPUT_PULLUP);
+  delayMicroseconds(50);  // Allow the resistors time to pull up the pins fully.
+}
+
 void setup() {
+  InitPins();
+
   auto teensy = std::make_unique<hs::TeensyImpl>();
   if (teensy->DigitalReadLow(hs::pins::kLeftOuter)) {
     hs::configurator::Configure(std::move(teensy));
